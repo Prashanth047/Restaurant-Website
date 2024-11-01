23602eca2f8bad64964b0150877793412c873479
Prashanth <guruprashanthg@gmail.com>
2024-11-01 12:14:17 +0530

    final commit

--- main.js
+++ main.js
@@ -10,3 +10,41 @@ menuBTN.addEventListener("click",(e)=>{
 
     menuBTNicon.setAttribute("class",isOpen?"ri-close-large-fill":"ri-menu-line");
 });
+
+navLinks.addEventListener("click",(e)=>{
+    navLinks.classList.remove("open");
+    menuBTN.setAttribute("class","ri-menu-line")
+})
+
+const scrollreveal={
+    distance: "50px",
+    origin:"bottom",
+    duration:1000,
+};
+
+ScrollReveal().reveal('.header_image img',{
+    ...scrollreveal,
+    origin:"right",
+});
+
+ScrollReveal().reveal('.header_content h2',{
+    ...scrollreveal,
+    delay:500,
+});
+
+ScrollReveal().reveal('.header_content h1',{
+    ...scrollreveal,
+    delay:1000,
+});
+
+ScrollReveal().reveal('.order_card',{
+    ...scrollreveal,
+    interval:500,
+});
+
+ScrollReveal().reveal('.event_content',{
+    duration:1000,
+});
+
+
+
